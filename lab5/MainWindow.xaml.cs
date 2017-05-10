1149d402263176f7f19a9ceefb9d881d3f30f3d3
Fejcvk <tomaszfejcak@icloud.com>
2017-05-10 10:18:46 +0200

    Dodaj pliki projektów.

---
+++ lab5/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace lab5
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public List<Movie> movies { get; set; }
+       
+        public MainWindow()
+        {
+            movies = new List<Movie>();
+            InitializeComponent();
+            //myGrid.ItemsSource = movies;
+            
+        }
+    }
+    public class Movie 
+    {
+        public string Title { get; set; }
+        public string Director { get; set; }
+        public Score Score { get; set;}
+        public MovieType Type { get; set;}
+
+    }
+
+    public enum Score { Terrible, Bad, OK, Good, Awesome };
+    public enum MovieType { Thriller, Comedy, Drama, Horror };
+}
